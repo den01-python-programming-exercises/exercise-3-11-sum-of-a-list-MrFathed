65ede698db8d407bcd1fa1b4c6c3364b1c361be9
Justin Turner <jmturner638@gmail.com>
2021-07-31 16:27:43 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #write your code below this line
+    numbers = []
+
+    while True:
+        number = int(input())
+
+        if number == -1:
+            break
+
+        numbers.append(number)
+
+    result = 0
+    for number in numbers:
+        result += number
+
+    print("Sum:", result)
 
 if __name__ == '__main__':
     main()
